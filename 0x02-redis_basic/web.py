b95928398e2ac3ba15bad584b7f48e81acc76d10
saad ouchama <contact@saadouchama.com>
2024-01-24 23:51:21 +0000

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
-(prototype: def get_page(url: str) -> str:). The core of
-the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it.
-
-Start in a new file named web.py and do not reuse the code
-written in exercise.py.
-
-Inside get_page track how many times a particular URL was
-accessed in the key "count:{url}" and cache the result with
-an expiration time of 10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
-
-
+"""
+web cache and tracker
+"""
 import requests
 import redis
 from functools import wraps
