ca953afd2a0b900579ea8d424dc0913feed60da5
saad ouchama <contact@saadouchama.com>
2024-01-24 23:21:17 +0000

    fix advanced task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -19,36 +19,33 @@ import redis
 import requests
 from functools import wraps
 
+# Create a Redis connection
 r = redis.Redis()
 
-
 def url_access_count(method):
-    """decorator for get_page function"""
+    """Decorator for the get_page function"""
     @wraps(method)
     def wrapper(url):
-        """wrapper function"""
+        """Wrapper function"""
         key = "cached:" + url
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
-
-            # Get new content and update cache
+        
+        # Get new content and update cache
         key_count = "count:" + url
         html_content = method(url)
-
+        
         r.incr(key_count)
         r.set(key, html_content, ex=10)
-        r.expire(key, 10)
         return html_content
     return wrapper
 
-
 @url_access_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
+    """Obtain the HTML content of a particular URL"""
     results = requests.get(url)
     return results.text
 
-
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.google.com'))
